2c763b5d269be93def60e8a67d5db10be52a0456
Hong <917777904@qq.com>
2024-08-04 22:02:41 +0800

    配置页面

--- src/renderer/src/views/Config.vue
+++ src/renderer/src/views/Config.vue
@@ -1,4 +1,43 @@
 <template>
-  <div class="h-screen bg-red-200 drag">dsdsdsdasasa</div>
+  <div
+    class="config h-screen text-gray-800 bg-gray-50 flex flex-col justify-between"
+  >
+    <div class="drag h-8 flex items-center px-2">
+      <CloseSmall theme="outline" class="cursor-pointer" />
+    </div>
+    <div class="flex-1 flex">
+      <ul class="w-20 mt-1 text-center text-lg">
+        <li>
+          <DocumentFolder theme="outline" />
+          <P>片段</P>
+        </li>
+        <li>
+          <SettingConfig theme="outline" />
+          <P>配置</P>
+        </li>
+      </ul>
+      <div
+        class="flex-1 mr-2 border-solid border border-slate-200 rounded-md bg-slate-50"
+      >
+        <router-view></router-view>
+      </div>
+    </div>
+    <div class="text-xs text-right py-1 mx-2">hong</div>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { CloseSmall, DocumentFolder, SettingConfig } from '@icon-park/vue-next'
+</script>
+
+<style lang="less" scoped>
+.config {
+  ul {
+    li {
+      @apply w-11/12 mx-auto mb-4 rounded-md cursor-pointer py-2 hover:bg-gray-200;
+      p {
+        @apply text-xs;
+      }
+    }
+  }
+}
+</style>
