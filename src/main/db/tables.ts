977afebe4ba6577abce83840c6dabb12310c3889
Hong <917777904@qq.com>
2024-09-17 23:15:17 +0800

    首次建表时自动新增’未分组‘

--- src/main/db/tables.ts
+++ src/main/db/tables.ts
@@ -1,42 +1,34 @@
 import { Database } from 'sqlite3'
-// import Mock from 'mockjs'
-// const Random = Mock.Random
-
 // 创建表
 const createTables = (db: Database) => {
   /**
    * 分组表
    * id，分组名称，创建时间，修改时间
    */
-  db.exec(`
+  db.run(
+    `
     CREATE TABLE IF NOT EXISTS categoryGroups (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       title TEXT NOT NULL,
       created_at TEXT NOT NULL,
       updated_at TEXT NOT NULL
     )
-  `)
-
-  // todo:首次应该自动创建未分组
-  // db.exec(`
-  //   insert into category (
-  //     id,
-  //     title,
-  //     created_at,
-  //     updated_at
-  //   ) values (
-  //     0,
-  //     '未分组',
-  //     datetime('now','localtime'),
-  //     datetime()
-  //   )
-  // `)
+  `,
+    function (e) {
+      if (e) throw e
+      db.get('SELECT * FROM categoryGroups', function (_err, row) {
+        if (!row) {
+          initCategoryGroup(db)
+        }
+      })
+    }
+  )
 
   /**
    * 内容表
    * id，片段名称，内容，所属分组，创建时间，修改时间
    */
-  db.exec(`
+  db.run(`
     CREATE TABLE IF NOT EXISTS contents (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       group_id INTEGER,
@@ -46,33 +38,23 @@ const createTables = (db: Database) => {
       updated_at TEXT NOT NULL
     )
   `)
+}
 
-  // for (let index = 0; index < 10; index++) {
-  //   db.exec(`
-  //   INSERT INTO categoryGroups (
-  //     title,
-  //     created_at,
-  //     updated_at
-  //   ) VALUES (
-  //     '${Random.ctitle(3, 5)}',
-  //     datetime('now','localtime'),
-  //     datetime()
-  //   )
-  // `)
-  //   db.exec(`
-  //   INSERT INTO contents (
-  //     title,
-  //     content,
-  //     created_at,
-  //     updated_at
-  //   ) VALUES (
-  //     '${Random.ctitle(3, 5)}',
-  //     '${Random.cparagraph(1, 2)}',
-  //     datetime('now','localtime'),
-  //     datetime()
-  //   )
-  // `)
-  // }
+// 初始化时自动新增'未分组'
+const initCategoryGroup = (db: Database) => {
+  db.run(`
+    insert into categoryGroups (
+      id,
+      title,
+      created_at,
+      updated_at
+    ) values (
+      0,
+      '未分组',
+      datetime('now','localtime'),
+      datetime()
+    )
+  `)
 }
 
 export default createTables
